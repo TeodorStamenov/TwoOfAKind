623d65e0ac76039f5d496babb54dbbfbfca1753f
Teodor Stamenov <teodor.stamenov@egt-bg.com>
2023-06-08 18:00:18 +0300

    fixed cards highlights

--- scripts/card.gd
+++ scripts/card.gd
@@ -6,6 +6,9 @@ signal on_flip_finish_signal
 
 @export var index = 0
 @onready var face = $Back/Face
+@onready var flipAnimation = $CardFlipAnimations
+
+@onready var initial_material = $Back.material
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,13 +19,14 @@ func _process(delta):
 	pass
 
 func _on_touch_screen_button_pressed():
-	$CardAnimations.play("card_flip")
+	flipAnimation.play("card_flip")
 	pass # Replace with function body.
 
 
 func _on_flip_animation_started(anim_name):
 	if anim_name == "card_flip":
 		$Button.visible = false
+		$Back.material = null
 		emit_signal("on_flip_started_signal", self)
 	pass # Replace with function body.
 
@@ -32,4 +36,5 @@ func _on_flip_animation_finished(anim_name):
 		emit_signal("on_flip_finish_signal")
 	elif anim_name == "card_hide":
 		$Button.visible = true
+		$Back.material = initial_material
 	pass # Replace with function body.
--- scripts/card_back.gd
+++ scripts/card_back.gd
@@ -2,20 +2,17 @@ extends TextureRect
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	get_node("Face").z_index = -1
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func run_highlight_effect():
-	get_parent().get_node("CardAnimations").play("card_highlight")
+	get_parent().get_node("CardHighlightAnimation").play("card_highlight")
 	pass
 
-
 func _on_highlight_timer_timeout():
-	var isCardFlipped = get_node("Face").z_index == 0
-	if not isCardFlipped:
-		run_highlight_effect()
+	run_highlight_effect()
 	pass # Replace with function body.
