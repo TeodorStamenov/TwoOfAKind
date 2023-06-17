9937d8927e3faeee0cf35f2a38940c5b7e91ffe8
teodorstamenov <teodor.m.stamenov@gmail.com>
2023-06-17 20:32:02 +0300

    fix star appear after matching

--- scripts/single_player_world.gd
+++ scripts/single_player_world.gd
@@ -23,19 +23,6 @@ func _on_timer_elapsed():
 	pass	
 	
 	
-func _trigger_star_fly():
-	var Star = preload("res://scenes/star.tscn")
-	var star = Star.instantiate()
-	add_child(star)
-	star.set_position(500, 600)
-	
-	var tween : Tween
-	tween = create_tween()
-	tween.tween_property(star, "position", $HUD/Star1.position, 1)
-	tween.finished.connect(_star_fly_end.bind(star))
-	pass
-	
-	
 func _star_fly_end(star):
 	$HUD/Star1.fill_star(points)
 	remove_child(star)
@@ -48,12 +35,12 @@ func _on_board_match_pairs():
 	
 	var first_star = Star.instantiate()
 	add_child(first_star)
-	first_star.position = $Board.first_card.face.global_position
+	first_star.global_position = $Board.first_card.face.global_position - first_star.pivot_offset
 	start_star_animation(first_star)
 	
 	var second_star = Star.instantiate()
 	add_child(second_star)
-	second_star.position = $Board.second_card.face.global_position
+	second_star.global_position = $Board.second_card.face.global_position - second_star.pivot_offset
 	start_star_animation(second_star)
 	
 	$Board.reset_cards()
@@ -62,13 +49,11 @@ func _on_board_match_pairs():
 
 func start_star_animation(star):
 	var final_scale = star.scale
-#	star.global_position.x -= (star.size.x / 2) * star.scale.x
-#	star.global_position.y -= (star.size.y / 2) * star.scale.y
 	
 	var tween : Tween
 	tween = create_tween()
-	tween.parallel().tween_property(star, "scale", Vector2(final_scale), 2).from(Vector2(0, 0))
-	tween.parallel().tween_property(star, "modulate", Color(1,1,1,1), 2).from(Color(1,1,1,0))
-	tween.chain().tween_property(star, "position", $HUD/Star1.position, 1).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_IN)
+	tween.parallel().tween_property(star, "scale", Vector2(final_scale), 0.250).from(Vector2(0, 0))
+	tween.parallel().tween_property(star, "modulate", Color(1,1,1,1), 0.250).from(Color(1,1,1,0))
+	tween.chain().tween_property(star, "position", $HUD/Star1.position, 0.500).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_IN)
 	tween.finished.connect(_star_fly_end.bind(star))
 	pass
